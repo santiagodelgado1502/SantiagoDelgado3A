8711986fd0cb793ca31c8d55ea70c5d7f5ef799b
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-04-28 20:13:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicacion = 0;
+            Console.WriteLine("Ingresar valor para hacer la tabla:");
+            int valor=int.Parse(Console.ReadLine());
+            for (int i = 0; i <= 15; i++)
+            {
+                multiplicacion = i * valor;
+                Console.WriteLine((i)+"  x  "+valor+"=  "+multiplicacion);
+            }
+            Console.ReadLine();
+        }
+    }
+}
