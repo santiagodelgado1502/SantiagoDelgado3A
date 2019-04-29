f3ecab110cb06a25b2f60bcd32a4fe41c5f08913
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-04-28 20:10:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Pago = 0,Extra=0,Extratotal=0,total=0;
+            Console.WriteLine("Ingresar horas trabajadas:");
+            int horas = int.Parse(Console.ReadLine());
+            if (horas <= 35)
+            {
+                total = horas * 15;
+            }
+            else
+            {
+                Extra = horas - 35;
+                Pago = 35 * 15;
+                Extratotal = Extra * 22;
+                total = Extra + Pago;
+
+            }
+            Console.WriteLine("La horas trabajadas son: "+horas);
+            Console.WriteLine("Horas extras trabajadas: "+Extra);
+            Console.WriteLine("Pago total: "+total);
+            Console.ReadLine();
+        }
+    }
+}
