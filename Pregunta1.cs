f894cae814b0a4075ee52cafa241de170e484db2
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-04-28 20:35:27 -0500

    Add files via upload

---
+++ Pregunta1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SantiagoDelgado3A.Tarea
+{
+
+    public class Pregunta1
+    {
+
+        public static void Main(String[] args)
+        {
+            int mayor = 0;
+            Console.WriteLine("Colocar la cantidad de numeros a ingresar: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Ingresar numero: ");
+                int valor = int.Parse(Console.ReadLine());
+                if (mayor < valor)
+                {
+                    mayor = valor;
+                }
+
+
+            }
+            Console.WriteLine("El numero mayor es: " + mayor);
+            Console.ReadLine();
+        }
+
+
+    }
+}
