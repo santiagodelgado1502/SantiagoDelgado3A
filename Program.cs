e1fa327e56d4ed79dfe5c3d801122b1398a41553
santiagodelgado1502 <50088704+santiagodelgado1502@users.noreply.github.com>
2019-04-28 20:11:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pregunta7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 100;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine((i+1)+"        "+contador);
+                contador--;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
